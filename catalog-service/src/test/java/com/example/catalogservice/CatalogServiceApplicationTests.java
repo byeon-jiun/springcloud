d53eb98aede936927be825326992296cc7851000
grey <askywsa@greenit.cc>
2023-03-27 13:39:23 +0900

    카탈로그 서비스 생성

--- catalog-service/src/test/java/com/example/catalogservice/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/example/catalogservice/CatalogServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.catalogsservice;
+package com.example.catalogservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CatalogsServiceApplicationTests {
+class CatalogServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
